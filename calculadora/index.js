c7916896955dc996be3afb7cc9571474b593bc17
alexbaaaa <aalejandrodesarrollo@gmail.com>
2024-08-05 15:39:20 +0200

    Add basic files

---
+++ calculadora/index.js
@@ -0,0 +1,46 @@
+// 
+
+console.log("Hola lola");
+
+let b00 = document.getElementById("b00");
+let b01 = document.getElementById("b01");
+let b02 = document.getElementById("b02");
+let b03 = document.getElementById("b03");
+let b04 = document.getElementById("b04");
+let b05 = document.getElementById("b05");
+let b06 = document.getElementById("b06");
+let b07 = document.getElementById("b07");
+let b08 = document.getElementById("b08");
+let b09 = document.getElementById("b09");
+
+
+b00.addEventListener("click", function(event){
+    console.log("0")
+})
+b01.addEventListener("click", function(event){
+    console.log("1")
+})
+b02.addEventListener("click", function(event){
+    console.log("2")
+})
+b03.addEventListener("click", function(event){
+    console.log("3")
+})
+b04.addEventListener("click", function(event){
+    console.log("4")
+})
+b05.addEventListener("click", function(event){
+    console.log("5")
+})
+b06.addEventListener("click", function(event){
+    console.log("6")
+})
+b07.addEventListener("click", function(event){
+    console.log("7")
+})
+b08.addEventListener("click", function(event){
+    console.log("8")
+})
+b09.addEventListener("click", function(event){
+    console.log("9")
+})
--- index.js
+++
@@ -1,2 +0,0 @@
-//Prueba del repositorio
-console.log("hola mundo");
